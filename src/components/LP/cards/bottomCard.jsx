0a04e78ca21de77b00f30732375667257c5fc62f
Kalel <ZoroPlay1220@gmail.com>
2025-06-23 14:30:37 -0300

    build

--- src/components/LP/cards/bottomCard.jsx
+++ src/components/LP/cards/bottomCard.jsx
@@ -1,11 +1,11 @@
-import './Cards.scss';
+import "./Cards.scss";
 export default function BottomCard({ title, desc, img, button }) {
-    return (
-        <div className='bottom-card'>
-            <h1>{title}</h1>
-            <img src={img} />
-            <p>{desc}</p>
-            <button>{button}</button>
-        </div>
-    );
+  return (
+    <div className="bottom-card">
+      <h1>{title}</h1>
+      <img src={img} />
+      <p>{desc}</p>
+      <button>{button}</button>
+    </div>
+  );
 }
--- src/components/LP/cards/topCard.jsx
+++ src/components/LP/cards/topCard.jsx
@@ -1,10 +1,10 @@
-import './Cards.scss';
+import "./Cards.scss";
 export default function TopCard({ title, desc, img }) {
-    return (
-        <div className='top-card'>
-            <h1>{title}</h1>
-            <img src={img} />
-            <p>{desc}</p>
-        </div>
-    );
+  return (
+    <div className="top-card">
+      <h1>{title}</h1>
+      <img src={img} />
+      <p>{desc}</p>
+    </div>
+  );
 }
